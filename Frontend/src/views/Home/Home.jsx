30424cfd50dfbcb2bc80aff027235da9e0076de8
t.zemmari <tarik.zemmari@xabec.es>
2021-03-26 16:26:58 +0100

    Still working in carousel

--- Frontend/src/views/Home/Home.jsx
+++ Frontend/src/views/Home/Home.jsx
@@ -13,40 +13,51 @@ import './Home.scss';
 
 const Home = (props) => {
 
-   const [latest, setLatest] = useState([])
-   const [populares, setPopulares] = useState([])
-   const [recomendaciones, setRecomendaciones] = useState([])
+   const [latest, setLatest] = useState([]);
+   const [latestPageTwo,setLatestPageTwo]=useState([]);
+   const [populares, setPopulares] = useState([]);
+   const [recomendaciones, setRecomendaciones] = useState([]);
 
    useEffect(() => {
 
       //Ultimas Peliculas
 
       let Latest = "https://api.themoviedb.org/3/movie/now_playing?api_key=ef2edc9da61e81787a8079a7df721936&language=en-US&page=1";
+      let LatestPageTow ="https://api.themoviedb.org/3/movie/now_playing?api_key=ef2edc9da61e81787a8079a7df721936&language=en-US&page=2";
 
       let populares = 'https://api.themoviedb.org/3/movie/popular?api_key=ef2edc9da61e81787a8079a7df721936&language=en-US&page=1';
 
       let recomendaciones = 'https://api.themoviedb.org/3/movie/top_rated?api_key=ef2edc9da61e81787a8079a7df721936&language=en-US&page=1'
 
-
+     //Populares
 
       fetch(populares)
          .then(res => (res.json()))
          .then(data => {
-            console.log(data)
+          
             setLatest(data.results)
          })
 
-      //Populares
+     
 
 
-
+       //Latest page1 and age2
 
       fetch(Latest)
          .then(res => (res.json()))
          .then(data => {
-            console.log(data)
+        
             setPopulares(data.results)
          })
+
+         
+      fetch(LatestPageTow)
+      .then(res => (res.json()))
+      .then(data => {
+        
+         setLatestPageTwo(data.results)
+      })
+      
 
       //Recomendaciones
 
@@ -55,9 +66,10 @@ const Home = (props) => {
       fetch(recomendaciones)
          .then(res => (res.json()))
          .then(data => {
-            console.log(data)
+          
             setRecomendaciones(data.results)
          })
+
 
 
 
@@ -81,33 +93,7 @@ const Home = (props) => {
       history.push('/peliculas')
    }
 
-   // Carousel 
-
-  /*const getRecommendMovies = () => {
-      fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=bb78e4cf3442e302d928f2c5edcdbee1`)
-          .then(res => res.json())
-          .then(data => {
-              let carousel = document.querySelector('.recommended-films .carousel');
-  
-              data.results.forEach(pelicula => {
-                  let imagePath = 'img/no-image.png';
-  
-                  if (pelicula.poster_path) {
-                      imagePath = `https://image.tmdb.org/t/p/w500${pelicula.poster_path}`;
-                  }
-  
-                  carousel.innerHTML += `<div class='film'><a href="#"><img src='${imagePath}' class='img-fluid float-end'></img></a></div>`
-                      // console.log(pelicula.poster_path);
-              });
-              //createPagination(data.results, document.querySelector('.recommended-films .indicators'));
-          });
-  };
-  
-attachCarouselEvents(document.querySelector('.recommended-films'));
-getRecommendMovies();*/
-      
-   // Fin Carousel
-      
+   
    return (
 
 
@@ -119,31 +105,34 @@ getRecommendMovies();*/
          <video className='myVideo' autoPlay muted loop id="myVideo" src={video}></video>
          <div class="content">
               
-              <p>AQUA-MAN</p>
-              <button id="myBtn" onClick={()=>GotoMovies()}> Mas información </button>
-             </div>
+             <h1 className='h1'></h1> 
+            <p>AQUA-MAN</p>
+            <button id="myBtn" onClick={()=>GotoMovies()}> Mas información </button>
+            </div>
             
-              <div className="separador"></div>
+            <div className="separador"></div>
             <h2 className='h2'>Ultimas Peliculas Añadidas</h2>
-
-            <Carousel>
-             {latest.map(latest => <Movie key={latest.id} {...latest} onClick={() => takeMeTo(latest)} />)} 
-             </Carousel>
-
             <div className="ultimas">
-               
+            <Carousel>
+               {latest.map(latest => <Movie key={latest.id} {...latest} onClick={() => takeMeTo(latest)} />)} 
+            </Carousel>
             </div>
-              <div className="separador"></div>
+
+            
+            <div className="separador"></div>
             <h2 className='h2'>Populares</h2>
-
             <div className="populares">
-               {populares.map(populares => <Movie key={populares.id} {...populares} onClick={() => takeMeTo(populares)} />)}
+            <Carousel>
+              {populares.map(populares => <Movie key={populares.id} {...populares} onClick={() => takeMeTo(populares)} />)}
+            </Carousel>
             </div>
-              <div className="separador"></div>
-            <h2 className='h2'>Recomendaciones</h2>
 
-            <div className="recomendadas">
+            <div className="separador"></div>
+            <h2 className='h2'>Recomendaciones</h2>
+            <div className="recomendaciones">
+            <Carousel>
                {recomendaciones.map(recomendaciones => <Movie key={recomendaciones.id} {...recomendaciones} onClick={() => takeMeTo(recomendaciones)} />)}
+            </Carousel>
             </div>
 
 
