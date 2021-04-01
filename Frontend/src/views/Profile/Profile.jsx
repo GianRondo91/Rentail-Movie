f5d0736daf8b75f10c18661da9669b1bd97f2d2e
keika <jesicamorocho.m@gmail.com>
2021-04-01 11:01:28 +0200

    adding logic to add favourite

--- Frontend/src/views/Profile/Profile.jsx
+++ Frontend/src/views/Profile/Profile.jsx
@@ -1,35 +1,52 @@
 import React, { useEffect, useState } from 'react';
-import {connect} from 'react-redux';
 import HeaderUser from '../../components/User/Header-user/Header-user';
 // import {useHistory} from 'react-router-dom';
 
 
-const Profile =(props)=>{
+const Profile =()=>{
+    
+    let key = "ef2edc9da61e81787a8079a7df721936";
+    let base_url = `http://api.themoviedb.org/3/movie/`;
+    let language = "language=es-ES"
+
+    const [favouritesMoviesId, setFavouritesMoviesId] = useState([])
+    const [allFavMovies, setAllFavMovies] = useState([])
+
+    useEffect(() => {
+        allFavouritesMovies()
+
+      },[]);
 
 
+    const allFavouritesMovies = () => {
+        const allFavourites = localStorage.getItem("favoritos")
+        
+        setFavouritesMoviesId(allFavourites)
+        console.log(favouritesMoviesId)
+        favouritesMoviesId.forEach(movie_id => {
+       
+        console.log(movie_id, 'id')
 
-   
-    // let history = useHistory();
+            fetch(`https://api.themoviedb.org/3/movie/${movie_id}?api_key=ef2edc9da61e81787a8079a7df721936&language=en`)
+            .then(res => (res.json()))
+            .then(data => {
 
-    let credentials=props.user.name;
-    console.log(credentials);
+            setAllFavMovies([data])
+            console.log(data)
+         })
+        })}   
+
+  
 
     return(
 
         <div className='component-profile'>
             <HeaderUser/>
-           
+            <p>{favouritesMoviesId}</p>
             
         </div>
     )
 }
 
 
-const mapStateToProps = state => {
-    return {
-        user : state.userReducer.user,
-        token : state.userReducer.token,
-       }
-}
-
-export default connect(mapStateToProps)(Profile);
+export default Profile;
