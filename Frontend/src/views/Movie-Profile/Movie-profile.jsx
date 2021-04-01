da2284ce359f39281fe01e5ce3a85c9b3ae1cfae
t.zemmari <tarik.zemmari@xabec.es>
2021-04-01 12:49:03 +0200

    movie-profile

--- Frontend/src/views/Movie-Profile/Movie-profile.jsx
+++ Frontend/src/views/Movie-Profile/Movie-profile.jsx
@@ -147,6 +147,7 @@ const MovieProfile = ({style}) => {
 
    )}else{
       return(
+      
          <div className='movie-profile-container'>
          <Header />
          <div className="movie-panel">
